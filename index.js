89cfe3d5af9875fa0bea5b5e4b3ea9faf5dddcd6
Megan Dacus <meganjustice0609@gmail.com>
2023-03-23 17:49:42 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+
+    function submitData(name1, email1) {
+        let p = document.createElement('p');
+        let body = document.querySelector('body');
+
+        return fetch ('http://localhost:3000/users', {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name1,
+            email: email1,
+        })
+    })
+        .then(resp => resp.json())
+
+        .then((json) => {
+            p.textContent = json.id;
+            body.appendChild(p);
+        })
+
+        .catch((e) => {
+            let message = e.message;
+            p.textContent = message;
+
+            body.appendChild(p);
+        })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
